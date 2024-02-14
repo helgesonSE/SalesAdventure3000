8a26d9e70e5af5897e0f87e44b27f0800413f8c5
helgesonSE <140814418+helgesonSE@users.noreply.github.com>
2024-02-14 10:40:41 +0100

    börjat på kartan

---
+++ SalesAdventure3000/Models/Models/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class Tile
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int[] Coordinates { get; set; }
+        public Color Color { get; set; }
+        public string Texture { get; set; }
+        public int? Occupant { get; set; }
+
+        public Tile(int[] coords, int? occupant) 
+        { 
+            Coordinates = coords;
+            Occupant = occupant;
+            
+            ID
+        }
+    }
+}
