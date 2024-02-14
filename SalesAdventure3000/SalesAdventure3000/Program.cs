5334534a25606b2e5e079030ee8c8cbaec27b261
helgesonSE <140814418+helgesonSE@users.noreply.github.com>
2024-02-14 09:59:15 +0100

    grundläggande projektstruktur

---
+++ SalesAdventure3000/SalesAdventure3000/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Models;
+
+namespace SalesAdventure3000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tile ruta = new Tile();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
